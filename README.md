7073ce9995fba0a17437f33e2789901a201a1282
kunzhang-oamk <c3zhku00@students.oamk.fi>
2024-01-30 20:44:29 +0200

    modified

--- README.md
+++
@@ -1,1 +0,0 @@
-# SendIt-backend
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import server from './app';
 
 const PORT = process.env.PORT || 4004;
 
-server.listen(PORT, () => {
+server.listen(3001, () => {
   // eslint-disable-next-line no-console
-  console.log(`app is listening on localhost:${PORT}`);
+  console.log(`app is listening on localhost:${3001}`);
 });
